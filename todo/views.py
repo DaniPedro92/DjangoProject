0d97213195192ab71ff10fc4cf75034cb9c3ff72
Filipe Daniel Pedro <filipedpedro@gmail.com>
2023-06-28 12:47:14 +0100

    app todo com listagem de tarefas

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Tarefa
 
 # Create your views here.
 def mostrar_tarefas(request):
-    #obter tarefas da base de dados!
-    #mostrar o template e enviar as tarefas
-    return render(request,"home.html")
+    # obter tarefas da base de dados!!
+    tarefas = Tarefa.objects.all().order_by('titulo')
+    # mostrar o template e enviar as tarefas
+    return render(request,"todo/home.html", {'tarefas':tarefas})
